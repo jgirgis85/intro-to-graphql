5ab8a27e23d9b9179c922424bfb7cb83143a6c18
Joe <j.girgis85@gmail.com>
2020-02-20 21:06:26 +0000

    yarn fix

--- src/db.js
+++ src/db.js
@@ -2,8 +2,11 @@ import mongoose from 'mongoose'
 import options from './config'
 
 export const connect = (url = options.dbUrl, opts = {}) => {
-  return mongoose.connect(
-    url,
-    { ...opts, useNewUrlParser: true }
-  )
+  return mongoose.connect(url, {
+    ...opts,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
 }
